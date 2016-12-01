295c0caad5c2e85955f4826f872c108cf76c11b4
lej5546 <lej5546@rit.edu>
2016-12-01 15:36:35 -0500

    Modified line search algorithm

--- sw/skynet/src/line_analyzer.c
+++ sw/skynet/src/line_analyzer.c
@@ -16,7 +16,12 @@ int AnalyzeLine(uint32_t* line, LineAnalyzerParams* params, LineFeatures* featur
 {
 	//uint32_t sample;
 	int i = 0;
-	int last_edge = 0;
+	//value at the last edge
+	int last_edge_val = 0;
+	//shift register of points with maximum difference
+	int edge_points[2] = [15,15]; //--------------------------------------------------------
+	//difference between resulting points from the edge finder
+	int point_diff = 0;
 
 	if (params->PrintDebug){
 		printf("%i\n\r",-1); // start value
@@ -27,33 +32,52 @@ int AnalyzeLine(uint32_t* line, LineAnalyzerParams* params, LineFeatures* featur
 		if (params->PrintDebug) {
 			printf("%i\n", line[i]);
 		}
-
+		// TODO: Sample offset?-------------------------------------------------------------------
 		int difference = line[i+params->SampleOffset] - line[i];
 
-		if ((abs(difference) > params->LineThreashold) &&
-				((i - last_edge) > params->LineTimeout)) {
+		if ((abs(difference) > last_edge_val {
 			// Okay, we found an edge
+			//shift right
+			edge_points[1] = edge_points[0];
+			edge_points[0] = i;
+			//set new max value
+			last_edge_val = abs(difference);
 
-			// Thoughts... should I be making sure I have the
-			// largest slope, as opposed to just any slope
-			// that matches the threashold?
-
-			if ((difference > 0) && !features->LeftLineVisible) {
-				// We found the left edge  __/--
-
-				features->LeftLineVisible = 1;
-				features->LeftLineLocation = i;
-				last_edge = i;
-				printf("Found left line at %d\n", i);
-
-			} else if ((difference < 0) && !features->RightLineVisible) {
-				// We found the right edge	--\__
-
-				features->RightLineVisible = 1;
-				features->RightLineLocation = i;
-				last_edge = i;
-				printf("Found right line at %d\n", i);
-			}
+		}
+	}
+	
+	point_diff = edge_points[0]-edge_points[1];
+	
+	if (point_diff < 50) {
+		// We have two different edges
+		features->RightLineVisible = 1;
+		features->LeftLineVisible = 1;
+		//Left edge in 0
+		if (edge_points[0] < params->LineLength/2){
+			features->RightLineLocation = edge_points[1];
+			features->LeftLineLocation = edge_points[0];
+			printf("Found right line at %d\n", edge_points[1]);
+			printf("Found left line at %d\n", edge_points[0]);
+		}
+		else{
+			features->RightLineLocation = edge_points[0];
+			features->LeftLineLocation = edge_points[1];
+			printf("Found right line at %d\n", edge_points[0]);
+			printf("Found left line at %d\n", edge_points[1]);
+		}
+	} else {
+		// We have one edge, use edge_points[1]
+		//Right edge
+		if (edge_points[1] < params->LineLength/2){
+			features->RightLineVisible = 1;
+			features->RightLineLocation = edge_points[1];
+			printf("Found right line at %d\n", edge_points[1]);
+		}
+		//Left edge
+		else{
+			features->LeftLineVisible = 1;
+			features->LeftLineLocation = edge_points[1];
+			printf("Found left line at %d\n", edge_points[1]);
 		}
 	}
 	if (params->PrintDebug) {
